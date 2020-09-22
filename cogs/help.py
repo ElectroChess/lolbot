cb2089882f9902ad9e78910c9a047b72878f8b5b
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-09-21 21:23:38 -0700

    e

--- cogs/help.py
+++ cogs/help.py
@@ -1,4 +1,6 @@
 import discord
+import random
+import datetime
 from discord.ext import commands
 
 class helpcmd(commands.Cog):
@@ -7,7 +9,8 @@ class helpcmd(commands.Cog):
     
     @commands.command()
     async def help(self, ctx):
-        embed=discord.Embed(title='lolbot Commands')
+        embed=discord.Embed(title='lolbot Commands', color=random.randint(0, 0xffffff), timestamp=datetime.datetime.utcnow())
+        embed.add_field(name='Meme Commands', value='`lol meme`, `lol cleanmeme`, `lol dankmeme`, `lol discordmeme`', inline=False)
         await ctx.send(embed=embed)
 
 def setup(bot):
--- cogs/meme.py
+++ cogs/meme.py
@@ -1,5 +1,5 @@
 import discord
-import requests
+import aiohttp
 import random
 import datetime
 from discord.ext import commands
@@ -10,29 +10,30 @@ class meme(commands.Cog):
     
     @commands.command(pass_context=True)
     async def meme(self, ctx):
-        r = requests.get('https://www.reddit.com/r/memes/hot.json?sort=new')
-        res = r.json()
-        ran = random.randint(0, 25)
-        imgurl = res['data']['children'][ran]['data']['url']
-        upvotes = res['data']['children'][ran]['data']['ups']
-        title = res['data']['children'][ran]['data']['title']
-        commments = res['data']['children'][ran]['data'][
-            'num_comments']
-        memeurl = res['data']['children'][ran]['data']['permalink']
-        awards = res['data']['children'][ran]['data'][
-            'total_awards_received']
-        author = res['data']['children'][ran]['data']['author']
-        embed = discord.Embed(
-            title=f"{title}",
-            url=f'https://reddit.com{memeurl}',
-            timestamp=datetime.datetime.utcnow(),
-            color=random.randint(0, 0xFFFFFF))
-        embed.set_image(url=imgurl)
-        embed.set_author(
-            name=author, url=f'https://reddit.com/u/{author}')
-        embed.set_footer(
-            text=f"👍 {upvotes} | 💬 {commments} | 🏆 {awards}")
-        await ctx.send(embed=embed)
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://www.reddit.com/r/memes/hot.json?sort=new') as r:
+                res = await r.json()
+                ran = random.randint(0, 25)
+                imgurl = res['data']['children'][ran]['data']['url']
+                upvotes = res['data']['children'][ran]['data']['ups']
+                title = res['data']['children'][ran]['data']['title']
+                commments = res['data']['children'][ran]['data'][
+                    'num_comments']
+                memeurl = res['data']['children'][ran]['data']['permalink']
+                awards = res['data']['children'][ran]['data'][
+                    'total_awards_received']
+                author = res['data']['children'][ran]['data']['author']
+                embed = discord.Embed(
+                    title=f"{title}",
+                    url=f'https://reddit.com{memeurl}',
+                    timestamp=datetime.datetime.utcnow(),
+                    color=random.randint(0, 0xFFFFFF))
+                embed.set_image(url=imgurl)
+                embed.set_author(
+                    name=author, url=f'https://reddit.com/u/{author}')
+                embed.set_footer(
+                    text=f"👍 {upvotes} | 💬 {commments} | 🏆 {awards}")
+                await ctx.send(embed=embed)
 
     
 
