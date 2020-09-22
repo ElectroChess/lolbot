af447042cb87133fa3fef9abd1e1e13a7f485657
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-09-21 20:42:14 -0700

    ummm

--- cogs/help.py
+++ cogs/help.py
@@ -8,7 +8,7 @@ class helpcmd(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         embed=discord.Embed(title='lolbot Commands')
-
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(helpcmd(bot))
