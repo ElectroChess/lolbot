31e59918075de4556ae600991fac04a1c94ecb2c
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-09-22 18:58:30 -0700

    token was moved

--- bot.py
+++ bot.py
@@ -31,4 +31,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run(open('token.txt').read())
+bot.run(open('C:/Users/Mythify/Desktop/tokens').read())
