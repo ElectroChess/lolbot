323947cb4f7b3122dad45c40aedc19dd51d05dd2
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-09-22 17:50:57 -0700

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -15,13 +15,14 @@ class other(commands.Cog):
     async def avatar(self, ctx, member: discord.Member=None):
         if not member:
             member = ctx.message.author
-        else:
-            show_avatar = discord.Embed(
-                description="[Avatar URL](%s)" % member.avatar_url,
-                timestamp=datetime.datetime.utcnow(),
-                color=random.randint(0, 0xFFFFFF))
-            show_avatar.set_image(url=f"{member.avatar_url}")
-            show_avatar.set_footer(text=f'{member}')
-            await ctx.send(embed=show_avatar)
+        
+        show_avatar = discord.Embed(
+            description="[Avatar URL](%s)" % member.avatar_url,
+            timestamp=datetime.datetime.utcnow(),
+            color=random.randint(0, 0xFFFFFF))
+        show_avatar.set_image(url=f"{member.avatar_url}")
+        show_avatar.set_footer(text=f'{member}')
+        await ctx.send(embed=show_avatar)
+
 def setup(bot):
     bot.add_cog(other(bot))
