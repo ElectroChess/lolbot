6cf1572fbddcc2c5239d237ef76eff343807adb1
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-09-23 19:18:21 -0700

    .gitignore

--- bot.py
+++ bot.py
@@ -31,4 +31,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run(os.environ.get('token'))
+bot.run(open('token.txt').read())
