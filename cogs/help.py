027327b23b70e7178912d43ee707fa4f36e5548c
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-10-01 08:35:55 -0700

    updated help command

--- cogs/help.py
+++ cogs/help.py
@@ -3,21 +3,29 @@ import random
 import datetime
 from discord.ext import commands
 
+
 class helpcmd(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def help(self, ctx):
-        embed=discord.Embed(title='lolbot Commands', color=random.randint(0, 0xffffff), timestamp=datetime.datetime.utcnow())
+        embed = discord.Embed(title='lolbot Commands', color=random.randint(
+            0, 0xffffff), timestamp=datetime.datetime.utcnow())
         embed.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
-        embed.add_field(name='Meme Commands', value='`lol meme`, `lol cleanmeme`, `lol dankmeme`, `lol discordmeme`', inline=False)
-        embed.add_field(name='Joke Commands', value='`lol joke`, `lol cleanjoke`', inline=False)
-        embed.add_field(name='Image Commands', value='`lol avatar`', inline=False)
+        embed.add_field(name='Meme Commands',
+                        value='`lol meme`, `lol cleanmeme`, `lol dankmeme`, `lol discordmeme`', inline=False)
+        embed.add_field(name='Joke Commands',
+                        value='`lol joke`, `lol cleanjoke`', inline=False)
+        embed.add_field(name='Image Commands',
+                        value='`lol avatar`, `lol yotub`', inline=False)
         embed.add_field(name='Fun Commands', value='`lol kill`', inline=False)
-        embed.add_field(name='Other Commands', value='`lol ping`, `lol invite`, `lol discord`', inline=False)
-        embed.set_footer(text=f'Requested by {ctx.author.name}', icon_url=ctx.author.avatar_url)
+        embed.add_field(name='Other Commands',
+                        value='`lol ping`, `lol invite`, `lol discord`', inline=False)
+        embed.set_footer(
+            text=f'Requested by {ctx.author.name}', icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(helpcmd(bot))
--- cogs/other.py
+++ cogs/other.py
@@ -3,23 +3,27 @@ import random
 import datetime
 from discord.ext import commands
 
+
 class other(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send(f'i responded in `{round(self.bot.latency * 1000)}`` milliseconds')
-    
+        await ctx.send(f'i responded in `{round(self.bot.latency * 1000)}` milliseconds')
+
     @commands.command()
     async def invite(self, ctx):
-        embed=discord.Embed(description='[**invite me pls**](https://tinyurl.com/lolbot-discordbot)', color=random.randint(0, 0xffffff))
+        embed = discord.Embed(
+            description='[**invite me pls**](https://tinyurl.com/lolbot-discordbot)', color=random.randint(0, 0xffffff))
         await ctx.send(embed=embed)
 
     @commands.command()
     async def discord(self, ctx):
-        embed=discord.Embed(description='[**join my dev server pls**](https://tinyurl.com/lolbot-server)', color=random.randint(0, 0xffffff))
+        embed = discord.Embed(
+            description='[**join my dev server pls**](https://tinyurl.com/lolbot-server)', color=random.randint(0, 0xffffff))
         await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(other(bot))
